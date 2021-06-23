af2f9981508c0a569509e7ff3411928e664690e5
Zach Thomas <zach.thomas2401@gmail.com>
2021-06-23 17:35:42 -0500

    Added userlist component

--- App.js
+++ App.js
@@ -7,9 +7,9 @@ import UserList from "./Components/UserList";
 export default function App() {
   return (
     <View style={styles.container}>
-      <UserList />
-      <StatusBar style="auto" />
+      <UserList style={styles.userList} />
       <Button title="button" />
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -17,8 +17,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 2,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
+  userList: {},
 });
--- Components/UserList.js
+++ Components/UserList.js
@@ -1,14 +1,38 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, FlatList } from "react-native";
 import dummyData from "./dummyData";
 
 const UserList = function () {
+  const Item = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+  const renderItem = ({ item }) => <Item title={item.userName} />;
   return (
     <View>
       <Text>UserList</Text>
+      <FlatList data={dummyData} renderItem={renderItem} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 20,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
 
 export default UserList;
