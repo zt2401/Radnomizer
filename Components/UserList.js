45a4d247c0af0ebc8871b4a7898d5a9a71c1610a
Zach Thomas <zach.thomas2401@gmail.com>
2021-06-23 16:52:29 -0500

    Added button and setting up userlist component

---
+++ Components/UserList.js
@@ -0,0 +1,14 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import dummyData from "./dummyData";
+
+const UserList = function () {
+  return (
+    <View>
+      <Text>UserList</Text>
+    </View>
+  );
+};
+
+export default UserList;
---
+++ Components/dummyData.js
@@ -0,0 +1,16 @@
+const dummyData = [
+  {
+    id: 1,
+    userName: "bojack",
+  },
+  {
+    id: 2,
+    userName: "vincent vega",
+  },
+  {
+    id: 3,
+    userName: "uma thruman",
+  },
+];
+
+export default dummyData;
